758f3c6c6e1aafb82e201efa7345f001fa9bc0f7
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-08-04 11:57:10 +0200

    Created the Button component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,21 @@
 <!-- Creating the App component -->
 <template>
-  <div class="app-container">App</div>
+  <div class="app-container">
+    <ButtonComponent text="Click me" />
+  </div>
 </template>
 
 <!-- Implementing JS -->
 <script>
+// Importing the costume components
+import ButtonComponent from "./ButtonComponent/ButtonComponent.vue";
+
 // Exporting the App component
 export default {
   name: "App",
+  components: {
+    ButtonComponent,
+  },
 };
 </script>
 
---
+++ src/components/ButtonComponent/ButtonComponent.vue
@@ -0,0 +1,26 @@
+<!-- Creating the button component -->
+<template>
+  <button @click="handleClick">{{ text }}</button>
+</template>
+
+<!-- Implementing JS -->
+<script>
+// Exporting the Button component
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: String,
+  },
+  methods: {
+    handleClick: () => {
+      console.log("clicked");
+    },
+  },
+};
+</script>
+
+<!-- Styling the Button component -->
+<style scoped>
+/* Importing the style file */
+@import url("./ButtonComponent.css");
+</style>
