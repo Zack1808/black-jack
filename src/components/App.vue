3ea326eebc7e32b233200493cb8e555e47ea713f
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-08-05 09:27:10 +0200

    Created the Card component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,14 @@
 <!-- Creating the App component -->
 <template>
   <div class="app-container">
-    <div class="ai-cards">
-      <h1 id="title" v-if="!gameActive">BlackJack</h1>
+    <h1 id="title" v-if="!gameActive">BlackJack</h1>
+    <div class="ai-cards" v-else>
+      <PlayCard
+        v-for="card in getPlayerCards"
+        :key="card.id"
+        :color="card.image"
+        :icon="card.icon"
+      />
     </div>
     <div class="buttons">
       <ButtonComponent v-if="!gameActive" @click="startGame" text="Play" />
@@ -10,7 +16,12 @@
       <ButtonComponent v-if="gameActive" @click="startGame" text="Stand" />
     </div>
     <div class="player-cards">
-      <div v-for="card in getPlayerCards" :key="card.id"></div>
+      <PlayCard
+        v-for="card in getPlayerCards"
+        :key="card.id"
+        :color="card.image"
+        :icon="card.icon"
+      />
     </div>
   </div>
 </template>
@@ -21,12 +32,14 @@ import { mapActions, mapGetters } from "vuex";
 
 // Importing the costume components
 import ButtonComponent from "./ButtonComponent/ButtonComponent.vue";
+import PlayCard from "./PlayCard/PlayCard";
 
 // Exporting the App component
 export default {
   name: "App",
   components: {
     ButtonComponent,
+    PlayCard,
   },
   methods: {
     ...mapActions(["startGame"]),
---
+++ src/components/PlayCard/PlayCard.vue
@@ -0,0 +1,33 @@
+<!-- Creating the card component -->
+<template>
+  <div class="card">
+    <div class="card-element">
+      <div class="top">
+        <span>{{ icon }}</span>
+        <img :src="color" alt="" />
+      </div>
+      <div class="middle">
+        <img .src="color" alt="" />
+      </div>
+      <div class="bottom">
+        <span>{{ icon }}</span>
+        <img :src="color" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Implementing JS -->
+<script>
+// Exporting the Card component
+export default {
+  name: "PlayCard",
+  props: ["icon", "color"],
+};
+</script>
+
+<!-- Styling the component -->
+<style scoped>
+/* Importing the style file */
+@import url("./PlayCard.css");
+</style>
